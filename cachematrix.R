d2a98db7bcc1e2a2e5b359448162ee8c485d6292
Josh Moffitt <joshmoff@gmail.com>
2014-06-22 12:57:40 -0700

    Assignment 2 ready for submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions will create an invertible matrix and allow the 
+## inverse of that matrix to be cached so it only has to be computed once.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix will create a matrix object that can cache it's inverse.
 makeCacheMatrix <- function(x = matrix()) {
+        #Initialize inverse variable on creation
+        inv_x <- NULL
+        
+        #Allows user to change the matrix and then clear the cache
+        set <- function(y) {
+                x <<- y
+                inv_x <<- NULL
+        }
+        
+        #Return current matrix
+        get <- function() x
+        
+        #Set inverse variable
+        set_inv <- function(inv) inv_x <<- inv
+        
+        #Return inverse variable
+        get_inv <- function() inv_x
+        
+        #Returns a list derived from the 4 functions above
+        list(set = set, 
+             get = get, 
+             set_inv = set_inv, 
+             get_inv = get_inv)
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve takes the list returned by makeCacheMatrix() as an input.
+## cacheSolve will first check to see if the inverse was already cached and
+## return that value.  Otherwise, it will compute the inverse, store it, and
+## return it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Initialize a local variable with the stored inverse.
+        inv_x <- x$get_inv()
+        
+        ## Check to see if an inverse was cached.
+        if(!is.null(inv_x)) {
+                message("getting cached inverse")
+                return(inv_X)
+        }
+        
+        ## Otherwise, compute inverse and cache it.
+        y <- x$get()
+        inv_x <- solve(y)
+        x$set_inv(inv_x)
+        
+        ## Return inverse
+        inv_x
 }
